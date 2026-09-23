f25336046a36594cea99499e83c9d616b338917e
LordChiiz <ahizechisom7@gmail.com>
2026-09-23 12:31:17 +0100

    feat: add Kalman filter
    Add a constant-velocity kalman filter for predicting object next position from its motion history and velocity estimation

---
+++ kalman_filter.py
@@ -0,0 +1,92 @@
+import numpy as np
+
+
+class kalman_filter:
+
+    def __init__(self, cx, cy, dt=1.0):
+        
+        # Define state [position, velocity, acceleration]
+        self.x = np.array([
+            [cx],
+            [cy],
+            [0.0],
+            [0.0]
+        ])
+
+        #motion model or state transition matrix
+        self.F = np.array([
+            [1.0, 0.0, dt, 0.0],
+            [0.0, 1.0, 0.0, dt,],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ])
+
+        #measurement model or observation matrix
+        self.H = np.array([
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0]
+        ])
+
+        #state uncertainty
+        self.P = np.eye(4) * 1.0
+
+        
+        #process noise
+        self.Q = np.eye(4) * 0.1
+
+
+        #measurement noise
+        self.R = np.array([
+            [1.0, 0.0],
+            [0.0, 1.0]
+        ])
+
+
+    def set_dt(self, dt):
+        self.F = np.array([
+            [1.0, 0.0, dt, 0.0],
+            [0.0, 1.0, 0.0, dt,],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ])
+
+
+
+    def predict(self):
+
+        #predict nxt states
+        self.x = self.F @ self.x
+
+        #predict new uncertainty
+        self.P = self.F @ self.P @ self.F.T + self.Q
+
+        return self.x
+    
+    def update(self, cx, cy):
+
+        z = np.array([
+            [cx],
+            [cy]
+        ])
+
+        #innovation or residual
+        y = z - self.H @ self.x
+
+
+
+        #calculate kalman gain
+        S = self.H @ self.P @ self.H.T + self.R
+        K = self.P @ self.H.T @ np.linalg.inv(S)
+
+        #update state
+        self.x = self.x + K @ y 
+
+        #reduce uncertainty
+        I = np.eye(self.P.shape[0])
+        self.P = (I - K @ self.H) @ self.P
+
+        return self.x
+
+
+
+
