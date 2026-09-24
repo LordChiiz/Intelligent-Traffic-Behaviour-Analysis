67c18b53187dfa959e2e4ea5ee5e6f5630f86d2a
LordChiiz <ahizechisom7@gmail.com>
2026-09-24 13:26:01 +0100

    Add IoU function
    Added IoU file in an attempt to replace reliance on ultralytics' built in BoT-SORT tracker with a from-scratch implementation to deomonstrate kalman filtering, bayesian estimation and iou-based association behind ' model.track()'

---
+++ IoU.py
@@ -0,0 +1,29 @@
+def iou(box_a, box_b):
+
+    xa1, ya1, xa2, ya2 = box_a
+    xb1, yb1, xb2, yb2 = box_b
+
+    inter_x1 = max(xa1, xb1)
+    inter_y1 = max(ya1, yb1)
+    inter_x2 = min(xa2, xb2)
+    inter_y2 = min(ya2, yb2)
+
+    inter_w = max(0.0, inter_x2 - inter_x1)
+    inter_h = max(0.0, inter_y2 - inter_y1)   
+
+    inter_area = inter_w * inter_h
+
+    area_a = max(0.0, xa2 - xa1) * max(0.0, ya2 - ya1)
+    area_b = max(0.0, xb2 - xb1) * max(0.0, yb2 - yb1)
+
+    union_area = area_a + area_b - inter_area
+
+    if union_area <= 0:
+        return 0.0
+    
+    return inter_area / union_area
+
+
+
+
+
